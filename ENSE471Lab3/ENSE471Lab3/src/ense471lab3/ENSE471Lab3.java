0bde9e24984c562d684b9a3a22d62920bc20215b
OriginalMidas <clayton.brez@gmail.com>
2017-02-22 14:35:40 -0600

    add components based on design requirements

---
+++ ENSE471Lab3/ENSE471Lab3/src/ense471lab3/ENSE471Lab3.java
@@ -0,0 +1,47 @@
+/*
+ Author: Clayton Brezinski
+ Student ID: 200220989
+ Notes:
+ */
+package ense471lab3;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+
+ @author Dolan
+ */
+public class ENSE471Lab3 extends javax.swing.JFrame
+{
+    private int MAX_HEIGHT = 800;
+    private int MAX_WIDTH = 800;
+    
+    public ENSE471Lab3()
+    {
+
+    }
+
+    /**
+     @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                new ENSE471Lab3().setVisible(true);
+            }
+        });
+
+    }
+}
+/*
+Purpose:
+Arguments:
+Outputs:
+Notes:
+*/
---
+++ ENSE471Lab3/ENSE471Lab3/src/ense471lab3/WeatherListener.java
@@ -0,0 +1,28 @@
+/*
+Author: Clayton Brezinski
+UserID: 200220989
+Date: 
+Notes:
+*/
+
+package ense471lab3;
+
+/*
+ *
+ * @author Dolan
+ */
+
+/*
+Purpose:
+Notes
+*/
+public class WeatherListener
+{
+
+}
+/*
+Purpose:
+Arguments:
+Outputs: 
+Notes:
+*/
