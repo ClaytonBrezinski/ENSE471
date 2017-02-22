07215cf941e110f9772e68b525ebc000900cf545
OriginalMidas <clayton.brez@gmail.com>
2017-02-22 17:21:59 -0600

    add weatherChange and temperatureChange methods

--- ENSE471Lab3/ENSE471Lab3/src/ense471lab3/WeatherSubscriber.java
+++ ENSE471Lab3/ENSE471Lab3/src/ense471lab3/WeatherSubscriber.java
@@ -1,24 +1,47 @@
 /*
-Author: Clayton Brezinski
-UserID: 200220989
-Date: 
-Notes:
-*/
-
+ Author: Clayton Brezinski
+ UserID: 200220989
+ Date:
+ Notes:
+ */
 package ense471lab3;
 
 /*
- *
- * @author Dolan
+
+ @author Dolan
  */
 
-/*
-Purpose:
-Notes
-*/
-public class WeatherSubscriber
+ /*
+ Purpose:
+ Notes
+ */
+public class WeatherSubscriber implements WeatherListener
 {
+    private int id;
 
+    public WeatherSubscriber(int inID)
+    {
+        this.id = inID;
+        System.out.println("WeatherSubscriber: " + inID + ", created");
+    }
+
+    @Override
+    public void weatherChange(WeatherEvent event)
+    {
+        System.out.println("LightWatcher-" + id
+                + ": I am notified that light is on");
+
+        if (event.temperature != 0 || event.weatherCondition != "")
+        {
+            System.out.println("LightWatcher-" + id + ": temperature: " + event.temperature + " ,weather: " + event.weatherCondition);
+        }
+    }
+
+    @Override
+    public void temperatureChange(WeatherEvent evt)
+    {
+
+    }
 }
 /*
 Purpose:
